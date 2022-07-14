cf5f8c1d68695b85be2aa707382e3cdea5c81b59
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-07-14 11:30:01 -0300

    login method with autorization

--- src/app/auth/services/auth-service.service.ts
+++ src/app/auth/services/auth-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
@@ -8,8 +9,13 @@ import { User } from '../models/user';
 export class AuthServiceService {
 
   constructor(private http: HttpClient) { }
+  private readonly baseUrl = 'http://localhost:8080'
 
-  signIn(user: User) {
-
+  signIn(user: User): Observable<{ Authorization: string }> {
+    return this.http.post<{ Authorization: string }>(`${this.baseUrl}/login`, user).pipe(
+      tap((response) => { this.armazenarToken(response.Authorization) }))
+  }
+  armazenarToken(token: string) {
+    localStorage.setItem('authorization', token)
   }
 }
